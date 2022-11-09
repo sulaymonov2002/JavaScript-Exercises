f1fd7f366dbdfafc7b88844aca178707103100c7
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-11-09 12:44:12 +0500

    add exercises-5 and answer

---
+++ JavaScript-Basic-150/Exercises-5/exercises-5.js
@@ -0,0 +1,36 @@
+// function animate_string(id) {
+//   const element = document.getElementById(id);
+//   const textNode = element.childNodes[0]; // boshqa bolalar yo'q deb hisoblasak
+//   let text = textNode.data;
+
+//   setInterval(() => {
+//     text = text[text.length - 1] + text.substring(0, text.length - 1);
+//     textNode.data = text;
+//   }, 1000);
+// }
+
+function animate_string(id) {
+  const element = document.getElementById(id);
+  const textNode = element.childNodes[0];
+  let text = textNode.data;
+
+  setInterval(() => {
+    text = text[text.length - 1] + text.substring(0, text.length - 1);
+    textNode.data = text;
+  }, 1000);
+}
+
+/**
+ * Tushuntirish:
+ *
+ * document.getElementById(id) : Elementga
+ *  identifikatori boʻyicha havolani qaytaradi;
+ *  identifikator elementni aniqlash uchun ishlatilishi mumkin bo'lgan qatordir;
+ *  u HTML-dagi id atributi yoki skript yordamida o'rnatilishi mumkin.
+ *
+ * Parametrlar : id - izlanayotgan elementning noyob identifikatorini
+ *  ifodalovchi katta-kichik harflarga sezgir satr.
+ *
+ * element.childNodes[0] birinchi tugunning
+ * HTML mazmuni bilan bir xil natija beradi.
+ */
