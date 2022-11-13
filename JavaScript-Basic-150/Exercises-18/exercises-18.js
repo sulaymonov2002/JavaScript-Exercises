d01de06d9e0dbe4929ad5a79430228156d17d7bc
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-11-13 19:33:10 +0500

    add new 18 exercises

---
+++ JavaScript-Basic-150/Exercises-18/exercises-18.js
@@ -0,0 +1,8 @@
+function test50(x, y) {
+  return x === 50 || y === 50 || x + y === 50;
+}
+
+console.log(test(50, 50));
+console.log(test(20, 50));
+console.log(test(20, 20));
+console.log(test(20, 30));
